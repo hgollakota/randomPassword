0a069ccb5845ff4cc32fefee1066bf4ca06cc61d
hgollakota <43627229+hgollakota@users.noreply.github.com>
2019-02-20 17:35:28 -0600

    change2

--- randompassword.py
+++ randompassword.py
@@ -5,11 +5,17 @@ import sys
 try:
 	passwordLength = int(sys.argv[1])
 except:
-	passwordLength = input("How long do you want your password? ")
+	while True:
+		try:
+			passwordLength = int(input("How long do you want your password? (positive integer) :"))
+			break
+		except:
+			pass
 
+	
 def randomPassword(length):
     shibboleth = ""
-    while (len(shibboleth) < int(length)):
+    while (len(shibboleth) < length):
         shibboleth += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
     return shibboleth
 
